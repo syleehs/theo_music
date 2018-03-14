02ff4258ac051ca01bd64a72b1242ba76289e250
Yannick Lee <lee.yannick@gmail.com>
2018-03-13 21:58:12 -0400

    Attempt fix

--- index.js
+++ index.js
@@ -74,19 +74,24 @@ Postback message processing
 ===================================================================*/
 // All callbacks for Messenger will be POST-ed here
 app.post("/webhook", function (req, res) {
+  let body = req.body
   // Make sure this is a page subscription
-  if (req.body.object == "page") {
+  if (body.object == "page") {
     // Iterate over each entry
     // There may be multiple entries if batched
-    req.body.entry.forEach(function(entry) {
+    body.entry.forEach(function(entry) {
+      let webhook_event = entry.messaging(0)
+      log.console(webhook_event)
       // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.postback) {
-          processPostback(event);
-        } else if (event.message) {
-          processMessage(event);
-        }
-      });
+      if (entry.messaging)  {
+        entry.messaging.forEach(function(event) {
+          if (event.postback) {
+            processPostback(event);
+          } else if (event.message) {
+            processMessage(event);
+          }
+        });
+      }
     });
 
     res.sendStatus(200);
